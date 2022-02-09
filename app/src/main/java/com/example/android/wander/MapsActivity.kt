0f4ab570eac66e1f0c7808c9ab565cc357aeb5d6
Wayno1979 <waynecockram@gmail.com>
2022-02-09 11:48:55 +0000

    added options to change map type

--- app/src/main/java/com/example/android/wander/MapsActivity.kt
+++ app/src/main/java/com/example/android/wander/MapsActivity.kt
@@ -2,6 +2,8 @@ package com.example.android.wander
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -43,5 +45,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val dawlish = LatLng(50.581209413973724, -3.4685453973626474)
         map.addMarker(MarkerOptions().position(dawlish).title("Marker in Dawlish Lawn"))
         map.moveCamera(CameraUpdateFactory.newLatLng(dawlish))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.map_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        // Change the map type based on the user's selection.
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
